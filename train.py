c00a26d67608fe5bb941fbfc801f9b141345b3ed
Rishabh Pathak <rishabh.pathak@leadsquared.com>
2023-11-09 17:13:49 +0530

    Made changes to log

--- train.py
+++ train.py
@@ -2,11 +2,11 @@ from pathlib import Path
 from dvclive import Live
 from random import random
 
-with Live(save_dvc_exp=True) as live:
+with Live(dir= "experiments/logs",save_dvc_exp=True,exp_name="Exp1") as live:
   for epoch in range(1,20):
     live.log_metric("accuracy", epoch*random())
     live.log_metric("loss",  epoch*random())
     live.next_step()
 
-live.log_param("Model","testing")
+live.log_param("Model","testing2")
 
